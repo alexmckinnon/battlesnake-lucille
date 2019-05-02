d527ae78c13d8eaee7c8aa60e19329c6da27188b
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-05-01 22:23:18 -0700

    Set up config and initial move strategy

--- app.js
+++ app.js
@@ -1,40 +1,69 @@
 'use strict'
 
-const fastify = require('fastify')({
+const config = {
+    snake: {
+        name: 'Lucille',
+        color: '#7d7d7c',
+        headType: 'safe',
+        tailType: 'round-bum'
+    },
+    output: {
+        board: true
+    },
     logger: {
-        prettyPrint: false 
+        prettyPrint: {
+            prettyPrint: true
+        }
     }
+}
+
+const fastify = require('fastify')({
+    logger: config.logger
 })
 
-fastify.get('/', function (request, reply) {
-    reply.send('Battlesnake')
+const strategies = require('./logic/strategy');
+const game = require('./utils/board');
+
+fastify.get('/', function (request, response) {
+    response.send('Battlesnake');
 })
 
 fastify.post('/start', (request, response) => {
-    response.send({
-        "color": "#736CCB",
-        "headType": "beluga",
-        "tailType": "curled"
-    })
+    response.send(config.snake);
 })
 
 fastify.post('/move', (request, response) => {
+
+    const board = game.gameBoard(request.body.board);
+    const me = request.you;
+
+    // Display board in console
+    if (config.output.board) {
+        console.log('board:');
+        board.forEach((row) => {
+            console.log(row.join(' | ') + ' |');
+        });
+    }
+
+    // Get move
+    const move = strategies.basic(board);
+
     response.send({
-        "move": "down"
-    })
+        "move": move
+    });
 })
 
 fastify.post('/end', (request, response) => {
-    response.send()
+    response.send();
 })
 
 fastify.post('/ping', (request, response) => {
-    response.send()
+    response.send();
 })
 
 fastify.listen(3000, (error, port) => {
     if (error) {
-        fastify.log.error(error)
-        process.exit(1) 
+        fastify.log.error(error);
+        process.exit(1);
     }
 })
