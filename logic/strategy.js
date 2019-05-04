acaac4e937340e9c84c5cbd08a54b5e6894492fc
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-05-04 12:41:16 -0700

    Update strategy to check for head on collisions

--- logic/strategy.js
+++ logic/strategy.js
@@ -2,9 +2,10 @@
  * Basic move algorithm - do some things and return a direction
  * @param {Object} board
  * @param {Object} me
+ * @param {Object} snakes
  * @returns String 
  */
-function basic(board, me) {
+function basic(board, me, snakes) {
 
     const head = me.body[0];
 
@@ -20,17 +21,22 @@ function basic(board, me) {
     if (moves.length === 1) {
         return moves[0].direction;
     }
+    
+    // Check if any moves are potential head-ons and adjust weight
+    // Todo: finish this << IMPORTANT
+    checkForHeadOnCollisons(moves, board, me, snakes);
 
     // Todo:
-    // Check if any moves are potential head-ons and adjust weight
     // Seek food
     // Seek heads of lesser snakes
-    //headOnCollisonCheck(moves, board);
+    // Avoid tight spaces (don't corner yourself) << IMPORTANT
 
     // Check if moves lead to dead ends and adjust weights
+    // Todo: dead end check doesn't account for if you could kill a snake by going this way and opening up a path
     checkForDeadEnds(moves, board);
 
     // Filter highest weighted moves
+    // Todo: use hunter/prey checks and favour hunter
     const weight = Math.max(...moves.map(move => move.weight), 0);
     const weightedMoves = moves.filter((move) => {
         return move.weight == weight;
@@ -51,17 +57,9 @@ function availableMoves(head, board) {
 
     let moves = [];
 
-    const options = [
-        {  direction: 'up',    x: head.x,      y: head.y - 1,  weight: 0  },
-        {  direction: 'down',  x: head.x,      y: head.y + 1,  weight: 0  },
-        {  direction: 'left',  x: head.x - 1,  y: head.y,      weight: 0  },
-        {  direction: 'right', x: head.x + 1,  y: head.y,      weight: 0  },
-    ]
+    const options = getValidSurroundingCoordinates(head.x, head.y, board);
     
     options.forEach((option) => {
-        if (outOfBounds(option.x, option.y, board.length - 1)) {
-            return;
-        }
         if (openSpace(option.x, option.y, board)) {
             option.weight += 1;
             moves.push(option);
@@ -98,6 +96,17 @@ function isFood(x, y, board) {
 }
 
 /**
+ * Check if x/y coordinates are a snake head
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Object} board
+ * @returns {Boolean}
+ */
+function isSnakeHead(x, y, board) {
+    return board[y][x] == 'H';
+}
+
+/**
  * Check of x or y is outside grid
  * @param {Number} x
  * @param {Number} y
@@ -116,25 +125,106 @@ function outOfBounds(x, y, max) {
  */
 function checkForDeadEnds(moves, board) {
     moves.forEach((move, index) => {
-        let nextMoves = [];
-        const nextOptions = [
-            {  x: move.x,      y: move.y - 1  },
-            {  x: move.x,      y: move.y + 1  },
-            {  x: move.x - 1,  y: move.y      },
-            {  x: move.x + 1,  y: move.y      },
-        ];
-
-        nextOptions.forEach((option) => {
-            if (!outOfBounds(option.x, option.y, board.length - 1)
-                && ( openSpace(option.x, option.y, board) || isFood(option.x, option.y, board) )) {
-                nextMoves.push(option);
-            }
-        })
-
+        let nextMoves = getValidNextMoves(move.x, move.y, board);
         if (nextMoves.length == 0) {
             moves[index].weight = 0;
         }
     });
+}
+
+/**
+ * Get array of open moves around given coordinates
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Object} board 
+ * @returns {Array}
+ */
+function getValidNextMoves(x, y, board) {
+
+    let nextMoves = [];
+    const nextOptions = getValidSurroundingCoordinates(x, y, board);
+
+    nextOptions.forEach((option) => {
+        if (openSpace(option.x, option.y, board) || isFood(option.x, option.y, board)) {
+            nextMoves.push(option);
+        }
+    });
+
+    return nextMoves;
+}
+
+/**
+ * Set the hunter/prey move properties if move has head on collision potential
+ * @param {Object} moves 
+ * @param {Object} board 
+ * @param {Object} me 
+ * @param {Object} snakes 
+ */
+function checkForHeadOnCollisons(moves, board, me, snakes) {
+    const myLength = me.body.length;
+    moves.forEach((move, index) => {
+        let surroudingCoordinates = getValidSurroundingCoordinates(move.x, move.y, board);
+        let hunter = false;
+        let prey = false;
+        let snakeHead = false;
+        surroudingCoordinates.forEach((move) => {
+            snakeHead = isSnakeHead(move.x, move.y, board);
+            if (snakeHead) {
+                let otherSnakeLength = getSnakeLength(move.x, move.y, snakes);
+                if (otherSnakeLength >= myLength) {
+                    prey = true;
+                }
+                if (otherSnakeLength < myLength) {
+                    hunter = true;
+                }
+            }
+        });
+        if (snakeHead) {
+            if (prey) {
+                moves[index].prey = true;
+                moves[index].weight = 0;
+            } else if (hunter) {
+                moves[index].hunter = true;
+                moves[index].weight += 1;
+            }
+        }
+    });
+}
+
+/**
+ * Get length of snake who's head is at x/y
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Object} snakes 
+ */
+function getSnakeLength(x, y, snakes) {
+    return snakes.find((snake) => snake.head = x + '.' + y).body.length;
+}
+
+/**
+ * Get array of surrounding coordinates that are not out of bounds
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Object} board 
+ * @returns {Array}
+ */
+function getValidSurroundingCoordinates(x, y, board) {
+
+    let inBounds = [];
+    const coordinates = [
+        {  direction: 'up',    x,         y: y - 1,  weight: 0,  hunter: false,  prey: false  },
+        {  direction: 'down',  x,         y: y + 1,  weight: 0,  hunter: false,  prey: false  },
+        {  direction: 'left',  x: x - 1,  y,         weight: 0,  hunter: false,  prey: false  },
+        {  direction: 'right', x: x + 1,  y,         weight: 0,  hunter: false,  prey: false  }
+    ];
+
+    coordinates.forEach((option) => {
+        if (!outOfBounds(option.x, option.y, board.length - 1)) {
+            inBounds.push(option);
+        }
+    });
+
+    return inBounds;
 }
 
 module.exports = { basic } 
